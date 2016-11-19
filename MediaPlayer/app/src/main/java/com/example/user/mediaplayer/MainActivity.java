b6d5b387dbc97b5c6bb8bdf8e4cb7403d5cceb94
adiarbib <adiarbib@gmail.com>
2016-11-19 18:43:51 +0200

    Adding Notification

--- MediaPlayer/app/src/main/java/com/example/user/mediaplayer/MainActivity.java
+++ MediaPlayer/app/src/main/java/com/example/user/mediaplayer/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.user.mediaplayer;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.SyncStateContract;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,13 +34,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        songsList=(ListView) findViewById(R.id.songsList);
-        playOrPause=(ImageButton)findViewById(R.id.playOrPause);
-        forawrd=(ImageButton)findViewById(R.id.forward);
-        backward=(ImageButton)findViewById(R.id.backward);
+        songsList = (ListView) findViewById(R.id.songsList);
+        playOrPause = (ImageButton) findViewById(R.id.playOrPause);
+        forawrd = (ImageButton) findViewById(R.id.forward);
+        backward = (ImageButton) findViewById(R.id.backward);
 
-        songArrayList=new ArrayList<>();
-        songsAdapter= new ArrayAdapter<Song>(this, android.R.layout.simple_list_item_1, songArrayList);
+        songArrayList = new ArrayList<>();
+        songsAdapter = new ArrayAdapter<Song>(this, android.R.layout.simple_list_item_1, songArrayList);
         songsList.setAdapter(songsAdapter);
         setSongsLists();
 
@@ -47,21 +54,31 @@ public class MainActivity extends AppCompatActivity {
         playOrPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                pauseSong();
+                if (Integer.parseInt(playOrPause.getTag().toString()) == R.drawable.play) {
+                    resumeSong();
+                } else {
+                    pauseSong();
+                }
+
+            }
+        });
+
+        playOrPause.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                stopSong();
+                return true;
             }
         });
 
         forawrd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(Integer.parseInt(playOrPause.getTag().toString())==R.drawable.play || currentSongPosition<0 || currentSongPosition>=songsList.getCount()-1)
-                {
-                    playSong(0);
-                }
-                else
-                {
+                if (Integer.parseInt(playOrPause.getTag().toString()) == R.drawable.play || currentSongPosition < 0 || currentSongPosition >= songsList.getCount() - 1) {
+                    playNext(0);
+                } else {
                     currentSongPosition++;
-                    playSong(currentSongPosition);
+                    playNext(currentSongPosition);
                 }
             }
         });
@@ -69,23 +86,21 @@ public class MainActivity extends AppCompatActivity {
         backward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("current song position",currentSongPosition+"");
-                if(Integer.parseInt(playOrPause.getTag().toString())==R.drawable.play || currentSongPosition<0 || currentSongPosition>songsList.getCount()-1)
-                {
-                    playSong(0);
-                }
-                else if(currentSongPosition==0)
-                {
-                    playSong(songsList.getCount()-1);
-                }
-
-                else
-                {
+                if (Integer.parseInt(playOrPause.getTag().toString()) == R.drawable.play || currentSongPosition < 0 || currentSongPosition > songsList.getCount() - 1) {
+                    playPrevious(0);
+                } else if (currentSongPosition == 0) {
+                    playPrevious(songsList.getCount() - 1);
+                } else {
                     currentSongPosition--;
-                    playSong(currentSongPosition);
+                    playPrevious(currentSongPosition);
                 }
             }
         });
+
+        //TODO: add notification here
+
+
+
     }
 
     private void pauseSong() {
@@ -107,11 +122,55 @@ public class MainActivity extends AppCompatActivity {
         playOrPause.setTag(R.drawable.pause);
     }
 
+    private void playPrevious(int position)
+    {
+        currentSongPosition=position;
+        Song song=songsAdapter.getItem(position);
+        Intent intent = new Intent(MainActivity.this, MediaService.class);
+        intent.setAction("ACTION_SKIP_PREV");
+        intent.putExtra("songID",song.getResId());
+        startService(intent);
+        playOrPause.setImageResource(R.drawable.pause);
+        playOrPause.setTag(R.drawable.pause);
+    }
+
+    private void playNext(int position)
+    {
+        currentSongPosition=position;
+        Song song=songsAdapter.getItem(position);
+        Intent intent = new Intent(MainActivity.this, MediaService.class);
+        intent.setAction("ACTION_SKIP_NEXT");
+        intent.putExtra("songID",song.getResId());
+        startService(intent);
+        playOrPause.setImageResource(R.drawable.pause);
+        playOrPause.setTag(R.drawable.pause);
+    }
+
+
     private void setSongsLists() {
         songsAdapter.add(new Song("Falling away with you",R.raw.muse_falling_away_with_you));
         songsAdapter.add(new Song("Madness",R.raw.muse_madness));
         songsAdapter.add(new Song("Hysteria",R.raw.muse_hysteria));
     }
+
+    private void stopSong()
+    {
+        Intent intent = new Intent(MainActivity.this, MediaService.class);
+        intent.setAction("ACTION_STOP");
+        startService(intent);
+        playOrPause.setImageResource(R.drawable.play);
+        playOrPause.setTag(R.drawable.play);
+    }
+
+    private void resumeSong()
+    {
+        Intent intent = new Intent(MainActivity.this, MediaService.class);
+        intent.setAction("ACTION_RESUME");
+        startService(intent);
+        playOrPause.setImageResource(R.drawable.pause);
+        playOrPause.setTag(R.drawable.pause);
+    }
+
 
 }
 
--- MediaPlayer/app/src/main/java/com/example/user/mediaplayer/MediaService.java
+++ MediaPlayer/app/src/main/java/com/example/user/mediaplayer/MediaService.java
@@ -1,10 +1,18 @@
 package com.example.user.mediaplayer;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 
 /**
  * Created by User on 17/11/2016.
@@ -12,11 +20,18 @@ import android.support.annotation.Nullable;
 public class MediaService extends Service {
 
     public MediaPlayer mediaPlayer;
-    public final String ACTION_PLAY = "ACTION_PLAY";
-    public final String ACTION_PAUSE = "ACTION_PAUSE";
+    public final static String ACTION_PLAY = "ACTION_PLAY";
+    public final static String ACTION_PAUSE = "ACTION_PAUSE";
+    public final static String ACTION_STOP="ACTION_STOP";
+    public final static String ACTION_SKIP_PREV="ACTION_SKIP_PREV";
+    public final static String ACTION_RESUME="ACTION_RESUME";
+    public final static String ACTION_SKIP_NEXT="ACTION_SKIP_NEXT";
+    public final static int NOTIFICATION_ID=1;
+    public static boolean isPlaying=false;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
         String action = intent.getAction();
         int resID = intent.getIntExtra("songID", 0);
             switch (action) {
@@ -25,7 +40,7 @@ public class MediaService extends Service {
                         throw new RuntimeException();}
                     else
                     {
-                        PlaySong(resID);
+                        playSong(resID);
                     }
                     break;
 
@@ -33,8 +48,69 @@ public class MediaService extends Service {
                     if(mediaPlayer.isPlaying())
                         mediaPlayer.pause();
                     break;
+
+                case ACTION_STOP:
+                    stopSong();
+                    break;
+
+                case ACTION_RESUME:
+                    mediaPlayer.start();
+                    break;
+
+                case ACTION_SKIP_NEXT:
+                    if (resID == 0) {
+                        throw new RuntimeException();}
+                    else
+                    {
+                        playSong(resID);
+                    }
+                    break;
+
+                case ACTION_SKIP_PREV:
+                    if (resID == 0) {
+                        throw new RuntimeException();}
+                    else
+                    {
+                        playSong(resID);
+                    }
+                    break;
+
         }
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void initNotification() {
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingNotificationIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Intent previousIntent = new Intent(this, MediaService.class);
+        previousIntent.setAction(ACTION_SKIP_PREV);
+        PendingIntent pendingPreviousIntent = PendingIntent.getService(this, 0, previousIntent, 0);
+
+        Intent playIntent = new Intent(this, MediaService.class);
+        playIntent.setAction(ACTION_RESUME);
+        PendingIntent pendingPlayIntent = PendingIntent.getService(this, 0, playIntent, 0);
+
+        Intent nextIntent = new Intent(this, MediaService.class);
+        nextIntent.setAction(ACTION_SKIP_NEXT);
+        PendingIntent pendingNextIntent = PendingIntent.getService(this, 0, nextIntent, 0);
+
+        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.icon);
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("Music Player")
+                .setContentText("Music")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(Bitmap.createScaledBitmap(icon, 128, 128, false))
+                .setContentIntent(pendingNotificationIntent)
+                .setOngoing(true)
+                .addAction(R.drawable.back, "Previous", pendingPreviousIntent)
+                .addAction(R.drawable.play, "Play", pendingPlayIntent)
+                .addAction(R.drawable.forward, "Next", pendingNextIntent)
+                .build();
+
+        startForeground(NOTIFICATION_ID, notification);
     }
 
     @Nullable
@@ -47,6 +123,7 @@ public class MediaService extends Service {
     public void onCreate() {
         super.onCreate();
         mediaPlayer=new MediaPlayer();
+        initNotification();
     }
 
     @Override
@@ -58,11 +135,22 @@ public class MediaService extends Service {
         mediaPlayer=null;
     }
 
-    void PlaySong (int resID)
+    void playSong(int resID)
     {
         if(mediaPlayer.isPlaying())
             mediaPlayer.stop();
         mediaPlayer = MediaPlayer.create(this, resID);
         mediaPlayer.start();
+        isPlaying=true;
     }
+
+    void stopSong()
+    {
+        if(mediaPlayer.isPlaying())
+            mediaPlayer.stop();
+        isPlaying=false;
+    }
+
+
+
 }
